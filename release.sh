12cf5383d099a05187f99a10868625eee94dca43
Fabian Thorand <f.thorand@gmail.com>
2017-04-04 13:44:19 +0200

    Created script for creating zip file releases on linux

---
+++ release.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Prepare and package a linux release
+
+cabal_file="kos-c.cabal"
+release_dir="release-packages"
+
+if [ ! -f "$cabal_file" ]; then
+    # require that this is run from the root of our stack project
+    echo "Not ran inside project root." >&2
+fi
+
+# create dir
+mkdir -p "$release_dir"
+
+# build project
+stack build
+if [ $? -ne 0 ]; then
+    echo "Error building project." >&2
+fi
+
+# find version
+VERSION=$(grep -oP 'version:\s*\K[0-9].[0-9].[0-9].[0-9]' "$cabal_file")
+
+echo "Packaging release v$VERSION"
+
+# create target directory
+package_name="kos-c-$VERSION-linux-$(uname -m)"
+package_dir="$release_dir/$package_name"
+mkdir -p "$package_dir"
+
+# copy stuff to target (hacky hard-coded path)
+cp ".stack-work/install/x86_64-linux/lts-8.5/8.0.2/bin/koscc" "$package_dir/"
+cp -r "kosc-prelude" "$package_dir/"
+
+# create zip
+pushd "$release_dir"
+zip -r "$package_name.zip" "$package_name"
+popd
+
+echo "Done!"
