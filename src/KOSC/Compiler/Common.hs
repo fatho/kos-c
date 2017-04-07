c5a41df8a226cb45da49e8f9de7d6ee74964aa10
Fabian Thorand <f.thorand@gmail.com>
2017-04-07 20:46:13 +0200

    Refactoring of compiler monad

--- src/KOSC/Compiler/Common.hs
+++ src/KOSC/Compiler/Common.hs
@@ -1,12 +1,23 @@
 module KOSC.Compiler.Common
   ( KOSCCompilerT
   , KOSCMessage
+  , enterModule
+  , enterDecl
   , module KOSC.Compiler.Monad
   , module KOSC.Compiler.Errors
   ) where
 
+import Text.PrettyPrint.ANSI.Leijen
+
 import KOSC.Compiler.Errors
 import KOSC.Compiler.Monad
+import qualified KOSC.Language.AST as AST
 
 type KOSCCompilerT = CompilerT MessageContent
 type KOSCMessage = CompilerMessage MessageContent
+
+enterModule :: MonadCompiler e m => AST.ModuleName -> m a -> m a
+enterModule modName = pushContext $ text "In module:" <+> pretty modName
+
+enterDecl :: MonadCompiler e m => AST.Ident -> m a -> m a
+enterDecl declName = pushContext $ text "In declaration:" <+> pretty declName
